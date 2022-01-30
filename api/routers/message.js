b02a8e39a45a60806379e251d7664a158afc61f1
emrekahraman <43474063+emre-kahraman@users.noreply.github.com>
2022-01-30 18:41:32 +0300

    add searchmessage

--- api/routers/message.js
+++ api/routers/message.js
@@ -12,6 +12,7 @@ router.post("/addMessage", auth, async(req, res) => {
         res.status(500);
     }
 })
+
 router.get("/findMessage/:conversation", auth, async(req, res) => {
     try{
         const messages = await message.find({
@@ -20,6 +21,21 @@ router.get("/findMessage/:conversation", auth, async(req, res) => {
         res.status(200).json(messages);
     }
     catch (error){
+        res.status(500);
+    }
+})
+
+router.get("/searchMessage/:text", async(req,res) => {
+    try{
+        const messages = await message.find({
+            text: { $in: [req.params.text]}
+        })
+        if(messages.length==0){
+            return res.status(500).json("text not found in messages.")
+        }
+        res.status(200).json(messages);
+    }
+    catch {
         res.status(500);
     }
 })
