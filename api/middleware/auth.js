4352f296f5fba92a8dea10d8a7674a2b381fbab2
emrekahraman <43474063+emre-kahraman@users.noreply.github.com>
2022-06-09 12:51:01 +0300

    Update to bearer
    Update token to bearer and add return if token is not valid

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -2,17 +2,19 @@ const jwt = require('jsonwebtoken');
 const config = process.env;
 
 const verifyToken = (req, res, next) => {
-    const token = req.headers["auth"];
+    var bearer = req.headers["authorization"];
+    bearer = bearer.split(" ");
+    const token = bearer[1];
     if(!token){
         return res.status(403).json("no token.");
     }
-    try {
-        const decode = jwt.verify(token, config.TOKEN_KEY);
-        req.user = decode;
-    } catch (error) {
-        return res.status(500);       
-    }
-    return next();
+    jwt.verify(token, config.TOKEN_KEY, (err, user) =>{
+        if(err){
+            return res.status(403).json(err);
+        }
+        req.token = user;
+        next();
+    });
 }
 
 module.exports = verifyToken;
